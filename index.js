4031acc905c051b60192d2d19b68d2dab2e076d6
jakeelwes <jake.elwes@blueyonder.co.uk>
2016-03-26 13:15:41 +0000

    access control

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
+  next()
+})
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
